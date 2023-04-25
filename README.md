090483b99b86d3102b8efcef21e76458c7f52948
Tibor Bodecs <mail.tib@gmail.com>
2023-04-25 16:33:19 +0200

    Updated readme and AWS tests

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
-## Feather AWS Component
+## Hummingbird extension with Soto for AWS
 
-A [Hummingbird](https://github.com/hummingbird-project/hummingbird) AWS component.
+A [Hummingbird](https://github.com/hummingbird-project/hummingbird) extension using [Soto](https://github.com/soto-project/soto) for AWS.
+
+## Usage 
+
+```swift
+import Hummingbird
+import HummingbirdAWS
+
+let app = HBApplication()
+
+// get default client
+let client = app.aws.client
+
+
+// set custom client
+app.aws.client = .init(
+    credentialProvider: .static(accessKeyId: "", secretAccessKey: ""),
+    retryPolicy: .noRetry,
+    middlewares: [],
+    options: .init(
+        requestLogLevel: .critical,
+        errorLogLevel: .critical
+    ),
+    httpClientProvider: .createNewWithEventLoopGroup(app.eventLoopGroup),
+    logger: app.logger
+)
+```
--- Tests/HummingbirdAWSTests/HummingbirdAWSTests.swift
+++ Tests/HummingbirdAWSTests/HummingbirdAWSTests.swift
@@ -1,11 +1,29 @@
 import XCTest
 import Hummingbird
 import HummingbirdAWS
+@testable import SotoCore
 
 final class HummingbirdAWSTests: XCTestCase {
 
-    func test() async throws {
+    func testGet() async throws {
         let app = HBApplication()
-        _ = app.aws.client
+        XCTAssertNotNil(app.aws.client.credentialProvider as? RuntimeSelectorCredentialProvider)
+    }
+    
+    func testSet() async throws {
+        let app = HBApplication()
+        app.aws.client = .init(
+            credentialProvider: .static(accessKeyId: "", secretAccessKey: ""),
+            retryPolicy: .noRetry,
+            middlewares: [],
+            options: .init(
+                requestLogLevel: .critical,
+                errorLogLevel: .critical
+            ),
+            httpClientProvider: .createNewWithEventLoopGroup(app.eventLoopGroup),
+            logger: app.logger
+        )
+        
+        XCTAssertNotNil(app.aws.client.credentialProvider as? StaticCredential)
     }
 }
